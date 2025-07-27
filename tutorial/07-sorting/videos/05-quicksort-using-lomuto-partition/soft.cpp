65aec55ea26c781f9b987b376b9001f43a5d6556
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-27 10:08:06 +0800

    sorting

--- tutorial/07-sorting/videos/05-quicksort-using-lomuto-partition/soft.cpp
+++ tutorial/07-sorting/videos/05-quicksort-using-lomuto-partition/soft.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 #include<vector>
+#include<utility>
 using namespace std;
 class QuickSort{
-    public: lomutoPartition
+    public: int lomutoPartition(vector<int>& arr, int low,int high){
+        int i=low-1;
+        int pivot=arr[high];
+        for(int j=low;j< high;j++){
+            if(arr[j]<=pivot){
+                i++;
+                swap(arr[i],arr[j]);
+            }
+        }
+        swap(arr[i+1],arr[high]);
+        return i+1;
+    }
+    void qs(vector<int>& arr, int low ,int high){
+        if(low<high){
+            int mid = lomutoPartition(arr,low,high);
+            qs(arr,low,mid-1);
+            qs(arr,mid+1,high);
+        }
+    }
+};
+int main(){
+    QuickSort soft;
+    vector<int> vec={3,2,52,1,3,1,5,4};
+    soft.qs(vec,0,vec.size()-1);
+    for(int num:vec){
+        cout << num<<endl;
+    }
+    return 0;
 }
